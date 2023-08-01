305a9579509c3ac9d5bdf74a821b26dcd4130434
do5do <do151015@gmail.com>
2023-08-01 17:22:57 +0900

    0801

---
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.frontcontroller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyView { // view를 처리하는 객체
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
