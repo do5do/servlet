da9f26ad4fb3be397344d9d19fa0a9ff160f6504
do5do <do151015@gmail.com>
2023-10-02 22:35:41 +0900

    frontController 확장

--- src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -4,13 +4,11 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import hello.servlet.web.frontcontroller.MyView;
 import hello.servlet.web.frontcontroller.v2.ControllerV2;
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.List;
 
 public class MemberSaveControllerV2 implements ControllerV2 {
     MemberRepository memberRepository = MemberRepository.getInstance();
